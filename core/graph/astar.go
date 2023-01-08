f4efbd2e61a404b4c641418ebca40bfd4759d975
Marius Merschformann <marius.merschformann@gmail.com>
2023-01-08 23:46:09 +0100

    Add initializer

--- core/graph/astar.go
+++ core/graph/astar.go
@@ -1,5 +1,9 @@
 package graph
 
+import (
+	"github.com/merschformann/flow-experiment-api/plugin"
+)
+
 type Node struct {
 	Edges []*Edge
 	ID    int
@@ -13,3 +17,14 @@ type Edge struct {
 type AStarSearch interface {
 	Search(start, goal *Node) []*Node
 }
+
+// NewAStarSearch returns a new a-star search instance.
+func NewAStarSearch() AStarSearch {
+	plugin.Connect(con, &newAStarSearch)
+	return newAStarSearch()
+}
+
+var (
+	con            = plugin.NewConnection()
+	newAStarSearch func() AStarSearch
+)
