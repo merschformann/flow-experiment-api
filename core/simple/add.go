bd1e7dc5bc4b2ef3f05ac940c0a36b34a6480c15
Marius Merschformann <marius.merschformann@gmail.com>
2023-09-24 23:45:46 +0200

    Adding simple func for performance tests

---
+++ core/simple/add.go
@@ -0,0 +1,15 @@
+package simple
+
+import "github.com/merschformann/flow-experiment-api/plugin"
+
+// SimpleAdd implements a simple addition to test the performance of the plugin
+// integration vs. a direct call.
+func SimpleAdd(a, b int) int {
+	plugin.Connect(con, &simpleAdd)
+	return simpleAdd(a, b)
+}
+
+var (
+	con       = plugin.NewConnection()
+	simpleAdd func(a, b int) int
+)
--- version.go
+++ version.go
@@ -1,3 +1,3 @@
 package root
 
-const Version = "v0.0.59"
+const Version = "v0.0.60"
