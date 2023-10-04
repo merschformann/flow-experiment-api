196a8318b6c7571ae409219da51b63148f68f9c5
Marius Merschformann <marius.merschformann@gmail.com>
2023-10-04 15:27:31 +0200

    Cleanup

--- core/simple/interface.go
+++ core/simple/interface.go
@@ -3,8 +3,7 @@ package simple
 import "github.com/merschformann/flow-experiment-api/plugin"
 
 var (
-	con              = plugin.NewConnection()
-	simpleAdd        func(a, b int) int
-	newNodeContainer func(count int) NodeContainer
-	newSimpleAdder   func() SimpleAdder
+	con            = plugin.NewConnection()
+	simpleAdd      func(a, b int) int
+	newSimpleAdder func() SimpleAdder
 )
