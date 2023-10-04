9235b47e04a8269793d7b989a0a146069e226bf6
Marius Merschformann <marius.merschformann@gmail.com>
2023-10-04 15:11:10 +0200

    Internalizing functionality

--- core/simple/add.go
+++ core/simple/add.go
@@ -6,16 +6,9 @@ type SimpleAdder interface {
 	Add(a, b int) int
 }
 
-type simpleAdder struct {
-}
-
-func (s simpleAdder) Add(a, b int) int {
-	return a + b
-}
-
 func NewSimpleAdder() SimpleAdder {
 	plugin.Connect(con, &newSimpleAdder)
-	return simpleAdder{}
+	return newSimpleAdder()
 }
 
 // SimpleAdd implements a simple addition to test the performance of the plugin
